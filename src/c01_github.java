2832c20bec1d44853ba792015d775af1ff01310e
sehertkl <seher.drtl@gmail.com>
2023-12-10 21:16:56 +0300

    2.Gün ilk versiyon

---
+++ src/c01_github.java
@@ -0,0 +1,8 @@
+public class c01_github {
+    public static void main(String[] args) {
+        int a=128;
+        int b=256;
+
+        System.out.println("iki sayının toplamı" + (a+b));
+    }
+}
